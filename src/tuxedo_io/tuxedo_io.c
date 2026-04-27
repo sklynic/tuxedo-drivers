07004d832db5ab47ee8d07b39251425644b9a730
sklynic <39230198+sklynic@users.noreply.github.com>
2026-04-27 23:02:14 +0800

    Update tuxedo_io.c
    use board name instead of sku (stock value is 0001)

--- src/tuxedo_io/tuxedo_io.c
+++ src/tuxedo_io/tuxedo_io.c
@@ -246,7 +246,7 @@ static void uw_id_tdp(void)
 	} else if (dmi_match(DMI_BOARD_NAME, "X5KK45xS_X5SP45xS")) {
 		tdp_min_defs = tdp_min_xxxx45xs;
 		tdp_max_defs = tdp_max_xxxx45xs;
-	} else if (dmi_match(DMI_PRODUCT_SKU, "X6xR55xK")) {
+	} else if (dmi_match(DMI_BOARD_NAME, "JIAOLONG Series-X6xR55xK")) {
 		tdp_min_defs = tdp_min_x6fr5xx;
 		tdp_max_defs = tdp_max_x6fr5xx;
 	} else if (dmi_match(DMI_BOARD_NAME, "X6AR55xU")) {
